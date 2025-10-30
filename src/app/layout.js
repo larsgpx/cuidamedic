7f8ca50a29fd458d047657704f9c130c5ff7481c
Larry Garcia <larsgpx@gmail.com>
2025-10-30 15:47:04 -0500

    whatsapp icon fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { GlobalLoader } from "@/components/GlobalLoader";
+import Image from "next/image";
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -20,6 +21,12 @@ export default function RootLayout({ children }) {
       >
         <GlobalLoader />
         {children}
+
+        <div className="fixed bottom-4 right-4 z-50">
+          <a href="https://wa.me/51992802065" target="_blank">
+            <Image src="/icons/whatsapp-icon.webp" alt="Cuidamedic" width={60} height={60} className="w-20 h-20" />
+          </a>
+        </div>
       </body>
     </html>
   );
